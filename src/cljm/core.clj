8c660997b51fab9acba31cfbe5b7048d8db6368f
Vyacheslav Mikushev <vyacheslav.mik.8@gmail.com>
2019-06-08 16:43:46 +0300

    Parse input

--- src/cljm/core.clj
+++ src/cljm/core.clj
@@ -1,22 +1,73 @@
 (ns cljm.core
   (:require [org.httpkit.server :as httpkit]
             [cheshire.core :as json]
+            [clojure.walk :as walk]
+            [ring.util.codec :as codec]
             [cljm.env :as env]
             [cljm.routes :as routes]
-            [cljm.error-code :as error-code]))
+            [cljm.error-code :as error-code]
+            [clojure.java.io :as io]))
 
-(routes/defroute :get "/_matrix/client/versions" (fn [req] (println 'versions req)))
+(routes/defroute :get "/_matrix/client/versions" (fn [_]
+                                                   {:versions ["r0.4.0"]}))
 
 (defn make-response [result]
   (-> result
       (assoc :headers {"Content-Type" "application/json"})
       (update :body json/generate-string)))
 
+(defmacro protect [& body]
+  (let [handler (last body)
+        handler (when (and (list? handler) (= (first handler) 'handler))
+                  handler)
+        body (if handler
+               (butlast body)
+               body)]
+  `(try
+     ~@body
+     (catch Throwable t#
+       (println t#)
+       ~@(if handler
+           (rest handler)
+           nil)))))
+
+(defmulti parse-params :request-method)
+
+(defmethod parse-params :get [req]
+  (when-let [qs (:query-string req)]
+    (protect
+     (walk/keywordize-keys (codec/form-decode qs))
+     (handler [false error-code/not-json]))))
+
+(defn parse-body [{:keys [body]}]
+  (protect
+    [true
+     (when body
+       (cond
+         (string? body)
+         (json/parse-string body keyword)
+
+         (instance? java.io.InputStream body)
+         (json/parse-stream (io/reader body) keyword)
+
+         :else
+         (throw (Exception. "Format not supported"))))]
+    (handler [false error-code/not-json])))
+
+(defmethod parse-params :post [req] (parse-body req))
+(defmethod parse-params :put  [req] (parse-body req))
+
 (defn handler [req]
   (make-response
    (if-let [route (routes/find-route (:request-method req) (:uri req))]
-     {:status 200
-      :body (apply route req)}
+     (protect
+      (let [[parsed? body] (parse-params req)]
+        (if parsed?
+          {:status 200
+           :body (route {:body body
+                         :request req})}
+          body))
+      (handler error-code/internal-error))
      error-code/route-not-found)))
 
 (defonce server (atom nil))
--- src/cljm/error_code.clj
+++ src/cljm/error_code.clj
@@ -9,3 +9,9 @@
   (make-error-code 404 "M_NOT_FOUND" message))
 
 (def route-not-found (not-found "Route not found"))
+
+(def internal-error (make-error-code 500
+                                     "M_INTERNAL_SERVER_ERROR"
+                                     "Internal server error"))
+
+(def not-json (make-error-code 400 "M_NOT_JSON" "Request did not contain valid JSON"))
